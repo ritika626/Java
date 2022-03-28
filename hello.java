2a17a26ff401d3110d2de266c20147b456950e8f
Ritika Sharma <O140005@ramco-systems.com>
2022-03-28 12:13:43 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,57 @@
+// import java.util.Scanner;
+
+/* class Hello {
+    public static void main(String[] args){
+        System.out.println("hello wrld");
+        System.out.print("hello wrld");
+    }
+} */
+// Add two numbers specify the type of data
+// class sum{
+//    public static void main(String args[]){
+//        int a=10;
+//        int b=25;
+//        int c=a+b;
+//        System.out.print(c);
+//    }
+// }
+// Take the input from user by using scanner ;
+/* class getInput{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        String str=s.nextLine();
+        System.out.println(str);
+
+        // int a, b;
+        // Scanner s = new Scanner(System.in);
+        // a=s.nextInt();
+        // b=s.nextInt();
+        // int c=a+b;
+        // System.out.println(c);
+    }
+} */
+// CHeck a no is even or odd
+/* class EvenOrOdd{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a =s.nextInt();
+        if(a%2==0){
+            System.out.print("no is even");
+        }
+        System.out.print("no is odd");
+    }
+} */
+// Check both the numbers are positive
+/* class PositiveNo{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a =s.nextInt();
+        int b=s.nextInt();
+        if(){
+
+        }
+        else{
+            
+        }
+    }
+} */
