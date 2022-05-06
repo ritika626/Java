45433d68b1b92552ac29322199934384a99ad315
Ritika Sharma <O140005@ramco-systems.com>
2022-05-06 14:01:19 +0530

    rest

--- hello.java
+++ hello.java
@@ -1,4 +1,4 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 /* class Hello {
     public static void main(String[] args){
@@ -47,11 +47,172 @@
         Scanner s=new Scanner(System.in);
         int a =s.nextInt();
         int b=s.nextInt();
-        if(){
-
+        if(a==b){
+            System.out.print("Both are equal");
         }
         else{
-            
+            System.out.print("Both are not equal");
+        }
+    }
+
+        if(a>0 && b>0){
+            System.out.print("Both are positive");
+        }
+        else{
+            System.out.print("Both are negative");
         }
     }
 } */
+
+/* class Something{
+    public static void main (String[] args) {
+        int a=50,b=20;
+        if(a>b)
+        {
+            if(a>100)
+                System.out.println("Ace");
+            if(b<100)
+                b=50;
+        }
+        else if(a==b)
+        {
+            System.out.println("King");
+        }
+        else 
+        {
+            System.out.println("Queen");
+        }
+        System.out.print(b);
+    }
+}
+ */
+
+// class Solution {
+/*
+  average of numbers
+ * public static void main(String[] args){
+ * Scanner s=new Scanner(System.in);
+ * String a=s.nextLine();
+ * int m1=s.nextInt();
+ * int m2=s.nextInt();
+ * int m3=s.nextInt();
+ * int average=(m1+m2+m3)/3;
+ * System.out.println(a);
+ * System.out.println(average);
+ * }
+ */
+/*
+ * public static void main (String[] args) {
+ * int i=0;
+ * while(i<10)
+ * {
+ * i=i+1;
+ * System.out.print(i);
+ * i=i+1;
+ * }
+ * }
+ */
+// public static void main(String[] args) {
+/*
+ * int x=5;
+ * int y=5;
+ * while((x==y))
+ * {
+ * System.out.println("Hello");
+ * x++;
+ * y++;
+ * }
+ */
+/*
+ * int i = 1;
+ * while (i < 5) {
+ * System.out.print(2 * i);
+ * i = i + 1;
+ * }
+ */
+/*
+ * int i=2;
+ * while(i<10)
+ * {
+ * System.out.print(i);
+ * i=i+2;
+ * }
+ */
+/*
+ * int i=2;
+ * while(i<10)
+ * {
+ * System.out.print(i);
+ * i*=2;
+ * }
+ */
+/* int i = 10;
+while (i > 0) {
+    if (i % 2 == 0) {
+        System.out.print(10 - i);
+    }
+    i--;
+} */
+//     }
+
+// }
+
+// check a no is prime or not
+class PrimeOrNot {
+    public static void main(String args[]) {
+        /*
+         * int n;
+         * Scanner s=new Scanner(System.in);
+         * n=s.nextInt();
+         * int div=2;
+         * while(div <= n-1){
+         * if(n % div == 0){
+         * System.out.print("No not prime");
+         * break;
+         * }
+         * else{
+         * System.out.println("Prime");
+         * }
+         * div=div+1;
+         * }
+         */
+        int i = 9;
+        while ((i ) > 0) {
+            System.out.print(i);
+            if (i % 5 == 0)
+                return;
+            i--;
+        }
+    }
+
+}
+
+/* print char 
+class Chtu{
+    public static void main(String args[]){
+        char c;
+        Scanner s=new Scanner(System.in);
+        c=s.next().charAt(0);
+        if(c>='a' && c<='z' ){
+            System.out.print("0");
+        }
+        else if(c>='A' && c<='Z'){
+            System.out.print("1");
+        }
+        else{
+            System.out.print("-1");
+        }
+    }
+} */
+
+class ki{
+    public static void main(String args[]){
+        int S,E,W;
+        Scanner s=new Scanner(System.in);
+        S =s.nextInt();
+        E=s.nextInt();
+        
+    }
+}
+
+
